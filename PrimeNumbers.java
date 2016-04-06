915253b5de6aed4c3c5acc65d8d436e2398df34c
Srinivasaganapathi <ssrinivasaganapathi@gmail.com>
2016-04-06 19:39:18 +0530

    Added files via upload

---
+++ PrimeNumbers.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		 Scanner s= new Scanner (System.in);
+	     int R1=s.nextInt(),R2=s.nextInt(),j,flag=1;
+	     for(int i=R1;i<=R2;i++)
+	     {
+	    	 flag=1;
+	    	 j=2;
+	    	 while(j<=i/2)
+	    	 {
+	    		 if(i%j==0)
+	    		 {
+	    			 flag=0;
+	    			 break;
+	    		 }
+	    		 
+	    		 j++;
+	    	 }
+	    	 
+	    	 if(flag!=0)
+	    	 {
+	    		 System.out.println(i);
+	    	 }
+	    	 
+	     }
+	     
+		
+		s.close();
+	}
+
+}
