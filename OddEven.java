5beb6b11d7281c9c21d9312de9548b2877389230
Srinivasaganapathi <ssrinivasaganapathi@gmail.com>
2016-04-06 10:48:39 +0530

    Added files via upload

---
+++ OddEven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s= new Scanner (System.in);
+		int val;
+		val=s.nextInt();
+		if(val%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+		
+		s.close();
+	}
+
+}
